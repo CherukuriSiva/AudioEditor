b9c8f22b2589aca7f0ffc5080b7e546d0d2e75f7
Siva <siva@plivo.com>
2017-07-28 17:15:23 +0530

    Testing

--- AudioEditor/AppDelegate.swift
+++ AudioEditor/AppDelegate.swift
@@ -12,9 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    //Testing
-    //Testing
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
